612dd547352885087414b278bf388c00fdeb0be2
Ricgo <richigodinezsanchez@gmail.com>
2026-01-13 18:30:53 -0600

    agent and main implementation

--- termostato_inteligente/main.py
+++ termostato_inteligente/main.py
@@ -8,3 +8,11 @@ def main():
     #que corra por 10 iteraciones, imprimiendo en cada paso:  
 
     #El estado actual (Temperatura). o La acción elegida por el agente. o El nuevo estado después de la acción.  
+    for i in range(10):
+        print("Iteración", i)
+        print("Estado actual:", env.temperature)
+        action = agent.act(env.temperature)
+        print("Acción:", action)
+        env.update(action)
+        print("Nuevo estado:", env.temperature)
+        print("\n")
